1d997d2f4363e292659cea1f2caacbb619940c5a
Joshua Middleton <middletime2012@gmail.com>
2021-09-28 19:11:34 -0400

    page updated

---
+++ 07_ClassesConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using _07_ClassesNew;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ClassesConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle firstVehicle = new Vehicle();
+
+            firstVehicle.Make = "Honda";
+            firstVehicle.Model = "Civic";
+            firstVehicle.Year = 2012;
+            firstVehicle.Type = VehicleType.car;
+            firstVehicle.Mileage = 150000;
+            firstVehicle.Color = "Grey";
+        }
+    }
+}
---
+++ 07_ClassesNew/ClassExamples.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ClassesNew
+{
+    public enum VehicleType { car,suv,truck}
+    public class Vehicle
+    {
+        // accessmodifer -- type -- propertyName-- {get; set;}
+        public string Make { get; set; }
+        public int  Year { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public double Mileage { get; set; }
+
+        public VehicleType Type { get; set; }
+       
+        public Vehicle()
+        {
+
+        }
+        public Vehicle(string make,string model,int year, double milage,string color, VehicleType type)
+        {
+            Make = make;
+            Year = year;
+            Model = model;
+            Color = color;
+            Mileage = milage;
+            Type = type;
+
+           
+        }
+
+
+    }
+    public class Person
+    {
+        //age
+        //height
+        //gender
+        //weight
+        //DateOfBirth
+        //firstName
+        //lastNmae
+
+        public string firstName { get; set; }
+
+    }
+}
